dece0e8a86adbde420ef6ad2440a4670b0910547
Vitoria Dias <80860267+DiasVitoria@users.noreply.github.com>
2021-09-16 18:23:34 -0300

    Add files via upload

---
+++ Lista_de_Exercicios_V_Python_para_Zumbis/Exercicio2.py
@@ -0,0 +1,9 @@
+cont = 0
+for i in range(9):
+  if i != 3:
+    for j in range(6):
+      cont += 1
+      print('oi')
+print(cont)
+
+
---
+++ Lista_de_Exercicios_V_Python_para_Zumbis/Exercicio4.py
@@ -0,0 +1,9 @@
+cont = 0
+for i in range(18644,33088):
+  if '2' in str(i) and '7' not in str(i):
+    cont += 1
+print(cont)
+
+
+
+
